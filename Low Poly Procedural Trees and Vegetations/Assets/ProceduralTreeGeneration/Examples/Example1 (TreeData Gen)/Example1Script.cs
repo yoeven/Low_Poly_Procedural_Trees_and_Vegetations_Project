03ef66089d4120f35a31bc3dace6fe4b8bf9d095
yoeven <yoevenkhem@gmail.com>
2018-02-05 19:19:29 +0700

    Created Examples

---
+++ Low Poly Procedural Trees and Vegetations/Assets/ProceduralTreeGeneration/Examples/Example1 (TreeData Gen)/Example1Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TreeGen;
+
+public class Example1Script : MonoBehaviour
+{
+    public TreeData data;
+    public int NumberOfTrees = 100;
+    List<GameObject> trees;
+
+    void Start()
+    {
+        trees = new List<GameObject>();
+
+        for (int i = 0; i < Mathf.RoundToInt(NumberOfTrees/2); i++)
+        {
+            for (int y = 0; y < Mathf.RoundToInt(NumberOfTrees / 2); y++)
+            {
+                TreeGeneratorManager.instance.RequestTree(data, new Vector3(i * 10, 0, y * 10), callback);
+            }
+        }
+    }
+
+    public void callback(GameObject g)
+    {
+        trees.Add(g);
+    }
+
+
+}
---
+++ Low Poly Procedural Trees and Vegetations/Assets/ProceduralTreeGeneration/Examples/Example2 (Dynamic Tree Gen)/Example2Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TreeGen;
+
+public class Example2Script : MonoBehaviour {
+
+    public int NumberOfTrees = 100;
+    List<GameObject> trees;
+
+    void Start()
+    {
+        trees = new List<GameObject>();
+
+        for (int i = 0; i < Mathf.RoundToInt(NumberOfTrees / 2); i++)
+        {
+            for (int y = 0; y < Mathf.RoundToInt(NumberOfTrees / 2); y++)
+            {
+                TreeData data = ScriptableObject.CreateInstance<TreeData>();
+                data.RandomiseParameters(1);
+                TreeGeneratorManager.instance.RequestTree(data, new Vector3(i * 10, 0, y * 10), callback);
+            }
+        }
+    }
+
+    public void callback(GameObject g)
+    {
+        trees.Add(g);
+    }
+}
